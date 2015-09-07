6caab01f12ceae78718d6689d45dc6b495fefab1
toby <tobytronics@gmail.com>
2015-09-07 15:15:37 +0100

    Add eslint using rules from xoxo

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 // Hapi allocates an available port if PORT is not set
-server.connection({ port: process.env.PORT });
+server.connection({port: process.env.PORT});
 
 server.route({
-    method: 'GET',
-    path: '/',
-    config: {
-      description: 'Test endpoint'
-    },
-    handler: function (request, reply) {
-      return reply({ message: 'Endpoint OK' });
-    }
+  method: 'GET',
+  path: '/',
+  config: {
+    description: 'Test endpoint'
+  },
+  handler: function (request, reply) {
+    return reply({message: 'Endpoint OK'});
+  }
 });
 
 server.start(function (err) {
-  if(err) {
+  if (err) {
     console.log('Error starting server', err);
   }
 
