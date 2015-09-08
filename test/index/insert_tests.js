fb1288b5322162911f39e0f1a38480ce3f39f6a9
chrisbrainsick <chrisbrainsick@gmail.com>
2015-09-08 11:23:17 +0100

    index to elasticsearch

--- test/index/insert_tests.js
+++ test/index/insert_tests.js
@@ -1,11 +1,41 @@
 'use strict';
 
-var index = require('../../lib/index');
+var elasticsearch = require('elasticsearch');
+var sinon = require('sinon');
+
+var expect = require('chai').expect;
 
 describe('Index', function () {
   describe('insert', function () {
+    var elasticStub;
+    var indexedObject;
+    var index;
+
+    before(function () {
+      elasticStub = sinon.stub(elasticsearch, 'Client', function () {
+        return {
+          index: function (args) {
+            indexedObject = args.body;
+
+            return new Promise(function (resolve) {
+              resolve();
+            });
+          }
+        };
+      });
+
+      index = require('../../lib/index');
+    });
+
+    after(function () {
+      elasticStub.restore();
+    });
+
     it('adds the object', function (done) {
-      index.insert({name: 'an object'}).then(function () {
+      var object = {name: 'an object'};
+
+      index.insert(object).then(function () {
+        expect(indexedObject).to.be.equal(object);
         done();
       }, done);
     });
