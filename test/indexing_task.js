1b59a3391ad60467137f58c6eb7ec321fcd29d64
chrisbrainsick <chrisbrainsick@gmail.com>
2015-09-09 18:30:38 +0100

    create and get indexing tasks from dynamodb

--- test/indexing_task.js
+++ test/indexing_task.js
@@ -2,10 +2,30 @@
 
 const _ = require('lodash');
 const sinon = require('sinon');
-
-const indexingTask = require('../lib/indexing_task');
+const proxyquire = require('proxyquire');
 
 const expect = require('chai').expect;
+
+class DynamoDBStub {
+  constructor() {
+    this._items = [];
+  }
+
+  putItem(itemParams, callback) {
+    this._items.push(itemParams);
+    callback();
+  }
+
+  getItem(query, callback) {
+    const item = this._items.find(function (i) {
+      return i.Item.id.S === query.Key.id.S;
+    });
+
+    callback(null, item);
+  }
+}
+
+const indexingTask = proxyquire('../lib/indexing_task', {'aws-sdk': {DynamoDB: DynamoDBStub}});
 
 describe('Indexing task', function () {
   let clock;
@@ -19,61 +39,57 @@ describe('Indexing task', function () {
     clock.restore();
   });
 
-  describe('taskID', function () {
-    it('is returned', function (done) {
-      indexingTask({name: 'an object'}).then(function (task) {
-        expect(task).to.have.property('taskID');
+  describe('create', function () {
+    it('assigns an id', function (done) {
+      indexingTask.create({name: 'an object'}).then(function (task) {
+        expect(task).to.have.property('id');
         done();
       }, done);
     });
 
-    it('is unique for each indexing task', function (done) {
+    it('assigns unique ids', function (done) {
       const indexingTasks = [];
 
       for (let i = 0, len = 1000; i < len; i++) {
-        indexingTasks.push(indexingTask({number: i}));
+        indexingTasks.push(indexingTask.create({number: i}));
       }
 
       Promise.all(indexingTasks).then(function (tasks) {
         const uniqueIDs = _.uniq(tasks, function (t) {
-          return t.taskID;
+          return t.id;
         });
 
         expect(uniqueIDs).to.have.length(indexingTasks.length);
         done();
       }, done);
     });
-  });
 
-  describe('createdAt', function () {
-    it('is returned', function (done) {
-      indexingTask({name: 'an object'}).then(function (task) {
+    it('assigns createdAt date', function (done) {
+      indexingTask.create({name: 'an object'}).then(function (task) {
         expect(task).to.have.property('createdAt');
         done();
       }, done);
     });
 
-    it('is an ISO format date string', function (done) {
-      indexingTask({name: 'an object'}).then(function (task) {
+    it('createdDate is an ISO format date string', function (done) {
+      indexingTask.create({name: 'an object'}).then(function (task) {
         expect(task).to.have.property('createdAt', testDate.toISOString());
         done();
       }, done);
     });
-  });
 
-  describe('objectID', function () {
-    it('is returned', function (done) {
-      indexingTask({name: 'an object'}).then(function (task) {
+    it('assigns an objectID', function (done) {
+      indexingTask.create({name: 'an object'}).then(function (task) {
         expect(task).to.have.property('objectID');
         done();
       }, done);
     });
 
-    it('is unique for each indexing task', function (done) {
+    it('assigns unique objectIDs', function (done) {
       const indexingTasks = [];
 
       for (let i = 0, len = 1000; i < len; i++) {
-        indexingTasks.push(indexingTask({number: i}));
+        indexingTasks.push(indexingTask.create({number: i}));
       }
 
       Promise.all(indexingTasks).then(function (tasks) {
@@ -82,6 +98,24 @@ describe('Indexing task', function () {
         });
 
         expect(uniqueIDs).to.have.length(indexingTasks.length);
+        done();
+      }, done);
+    });
+  });
+
+  describe('get', function () {
+    let createdTask;
+
+    before(function (done) {
+      indexingTask.create({name: 'an object'}).then(function (task) {
+        createdTask = task;
+        done();
+      }, done);
+    });
+
+    it('retrieves task information by id', function (done) {
+      indexingTask.get(createdTask.id).then(function (task) {
+        expect(task).to.be.eql(createdTask);
         done();
       }, done);
     });
