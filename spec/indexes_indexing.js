6e27636f23c4134466670255ec2b1cf786492240
Chris Impey <chris@bigwednesday.io>
2015-09-11 10:26:44 +0100

    rename helper function

--- spec/indexes_indexing.js
+++ spec/indexes_indexing.js
@@ -3,7 +3,7 @@
 const cuid = require('cuid');
 
 const elasticsearchClient = require('../lib/elasticsearchClient');
-const requestWrapper = require('./request_wrapper');
+const specRequest = require('./spec_request');
 
 const expect = require('chai').expect;
 
@@ -18,7 +18,7 @@ describe('Indexes', function () {
     it('accepts a new object', function () {
       const payload = {name: 'object', field: 'value'};
 
-      return requestWrapper({url: '/1/indexes/' + testIndexName, method: 'post', payload: payload})
+      return specRequest({url: '/1/indexes/' + testIndexName, method: 'post', payload: payload})
         .then(function (response) {
           expect(response.statusCode).to.equal(200);
         });
