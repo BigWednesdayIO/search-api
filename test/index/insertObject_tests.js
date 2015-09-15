b6f897baefe81de47f229e692d275f6c2507376d
Chris Impey <chris@bigwednesday.io>
2015-09-15 16:25:30 +0100

    make method names consistant - we're not inserting an index, but inserting an object into an index

--- test/index/insertObject_tests.js
+++ test/index/insertObject_tests.js
@@ -11,7 +11,7 @@ const testIndexName = 'my-index-name';
 const testObject = {name: 'an object', field1: 'value', field2: 0};
 
 describe('Index', () => {
-  describe('insert', () => {
+  describe('insertObject', () => {
     let elasticStub;
     let indexedObject;
     let index;
@@ -36,19 +36,19 @@ describe('Index', () => {
     });
 
     it('adds the object to elasticsearch', () => {
-      return index.insert(testObject).then(() => {
+      return index.insertObject(testObject).then(() => {
         expect(indexedObject).to.be.equal(testObject);
       });
     });
 
     it('writes to the named index', () => {
-      return index.insert(testObject).then(() => {
+      return index.insertObject(testObject).then(() => {
         expect(indexName).to.be.equal(testIndexName);
       });
     });
 
     it('returns the fields from the indexed object', () => {
-      return index.insert(testObject).then(o => {
+      return index.insertObject(testObject).then(o => {
         _.forOwn(testObject, (value, property) => {
           expect(o).to.have.property(property, value);
         });
@@ -56,7 +56,7 @@ describe('Index', () => {
     });
 
     it('returns the generated objectID', () => {
-      return index.insert(testObject).then(o => {
+      return index.insertObject(testObject).then(o => {
         expect(o.objectID).to.be.equal('123');
       });
     });
