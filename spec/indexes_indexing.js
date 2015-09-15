476ca4c29494578dd79f2158b380b707fed9b2a5
Chris Impey <chris@bigwednesday.io>
2015-09-15 11:58:46 +0100

    create spec for get object request

--- spec/indexes_indexing.js
+++ spec/indexes_indexing.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash');
 const cuid = require('cuid');
 
 const elasticsearchClient = require('../lib/elasticsearchClient');
@@ -7,20 +8,36 @@ const specRequest = require('./spec_request');
 
 const expect = require('chai').expect;
 
-describe('Indexes', function () {
-  describe('indexing', function () {
+describe('Indexes', () => {
+  describe('indexing', () => {
+    let createResponse;
+    const testObject = {name: 'object', field: 'value'};
     const testIndexName = 'test_index_' + cuid();
 
-    afterEach(function () {
+    before(() => {
+      return specRequest({url: '/1/indexes/' + testIndexName, method: 'post', payload: testObject})
+        .then(response => {
+          createResponse = response;
+        });
+    });
+
+    after(() => {
       return elasticsearchClient.indices.delete({index: testIndexName});
     });
 
-    it('accepts a new object', function () {
-      const payload = {name: 'object', field: 'value'};
+    it('accepts a new object', () => {
+      expect(createResponse.statusCode).to.equal(201);
+    });
 
-      return specRequest({url: '/1/indexes/' + testIndexName, method: 'post', payload: payload})
-        .then(function (response) {
-          expect(response.statusCode).to.equal(201);
+    it('allows indexed objects to be retrieved', () => {
+      return specRequest({url: createResponse.headers.location})
+        .then(response => {
+          expect(response.statusCode).to.equal(200);
+          expect(response.result.objectID).to.equal(createResponse.result.objectID);
+
+          _.forOwn(testObject, (value, property) => {
+            expect(response.result).to.have.property(property, value);
+          });
         });
     });
   });
