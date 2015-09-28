1a12e3870fb754a2cd57b0824090902919bea1e4
toboid <toboid@users.noreply.github.com>
2015-09-28 15:12:37 +0100

    Prevent wildcards in index names

--- test/search/basic_search_tests.js
+++ test/search/basic_search_tests.js
@@ -45,203 +45,182 @@ describe('Search', () => {
       });
   });
 
+  it('prevents index wildcards', () => {
+    return search.query(indexName, {})
+      .then(() => {
+        expect(searchArgs.expandWildcards).to.equal('none');
+        expect(searchArgs.allowNoIndices).to.equal(false);
+      });
+  });
+
+  it('queries requested index', () => {
+    return search.query(indexName, {})
+      .then(() => expect(searchArgs.index).to.equal(indexName));
+  });
+
   it('builds a default match all query when no search is supplied', () => {
-    const expected = {
-      index: indexName,
-      body: {query: {filtered: {}}, size: 10}
+    const expectedQuery = {
+      query: {
+        filtered: {}
+      },
+      size: 10
     };
 
     return search.query(indexName, {})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds a fuzzy keyword query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {query: {simple_query_string: {query: 'some-keyword~1'}}}},
-        size: 10
-      }
+    const expectedQuery = {
+      query: {filtered: {query: {simple_query_string: {query: 'some-keyword~1'}}}},
+      size: 10
     };
 
     return search.query(indexName, {query: 'some-keyword'})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds a fuzzy multi keyword query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {query: {simple_query_string: {query: 'keyword1~1 keyword2~1'}}}},
-        size: 10
-      }
+    const expectedQuery = {
+      query: {filtered: {query: {simple_query_string: {query: 'keyword1~1 keyword2~1'}}}},
+      size: 10
     };
 
     return search.query(indexName, {query: 'keyword1 keyword2'})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds a term filtered query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {
-          filtered: {
-            filter: {and: [{term: {field1: 'term1'}}, {term: {field2: 'term2'}}]}
-          }
-        },
-        size: 10
-      }
+    const expectedQuery = {
+      query: {
+        filtered: {
+          filter: {and: [{term: {field1: 'term1'}}, {term: {field2: 'term2'}}]}
+        }
+      },
+      size: 10
     };
 
     return search.query(indexName, {
       filters: [{field: 'field1', term: 'term1'}, {field: 'field2', term: 'term2'}]
     })
-    .then(() => expect(searchArgs).to.deep.equal(expected));
+    .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds a range filtered query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {
-          filtered: {
-            filter: {and: [{range: {field1: {gte: 1, lte: 2}}}]}
-          }
-        },
-        size: 10
-      }
+    const expectedQuery = {
+      query: {
+        filtered: {
+          filter: {and: [{range: {field1: {gte: 1, lte: 2}}}]}
+        }
+      },
+      size: 10
     };
 
     return search.query(indexName, {
       filters: [{field: 'field1', range: {from: 1, to: 2}}]
     })
-    .then(() => expect(searchArgs).to.deep.equal(expected));
+    .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds lower bound only range filtered query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {
-          filtered: {
-            filter: {and: [{range: {field1: {gte: 1}}}]}
-          }
-        },
-        size: 10
-      }
+    const expectedQuery = {
+      query: {
+        filtered: {
+          filter: {and: [{range: {field1: {gte: 1}}}]}
+        }
+      },
+      size: 10
     };
 
     return search.query(indexName, {
       filters: [{field: 'field1', range: {from: 1}}]
     })
-    .then(() => expect(searchArgs).to.deep.equal(expected));
+    .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds upper bound only range filtered query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {
-          filtered: {
-            filter: {and: [{range: {field1: {lte: 5}}}]}
-          }
-        },
-        size: 10
-      }
+    const expectedQuery = {
+      query: {
+        filtered: {
+          filter: {and: [{range: {field1: {lte: 5}}}]}
+        }
+      },
+      size: 10
     };
 
     return search.query(indexName, {
       filters: [{field: 'field1', range: {to: 5}}]
     })
-    .then(() => expect(searchArgs).to.deep.equal(expected));
+    .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds upper bound only range filtered query', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {
-          filtered: {
-            filter: {and: [{range: {field1: {lte: 5}}}]}
-          }
-        },
-        size: 10
-      }
+    const expectedQuery = {
+      query: {
+        filtered: {
+          filter: {and: [{range: {field1: {lte: 5}}}]}
+        }
+      },
+      size: 10
     };
 
     return search.query(indexName, {
       filters: [{field: 'field1', range: {to: 5}}]
     })
-    .then(() => expect(searchArgs).to.deep.equal(expected));
+    .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('defaults to page size of 10', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {}},
-        size: 10
-      }
+    const expectedQuery = {
+      query: {filtered: {}},
+      size: 10
     };
 
     return search.query(indexName, {})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds query with modified page size', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {}},
-        size: 50
-      }
+    const expectedQuery = {
+      query: {filtered: {}},
+      size: 50
     };
 
     return search.query(indexName, {hitsPerPage: 50})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds query with page number', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {}},
-        size: 10,
-        from: 10
-      }
+    const expectedQuery = {
+      query: {filtered: {}},
+      size: 10,
+      from: 10
     };
 
     return search.query(indexName, {page: 2})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds sorted query with default order', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {}},
-        size: 10,
-        sort: ['field1']
-      }
+    const expectedQuery = {
+      query: {filtered: {}},
+      size: 10,
+      sort: ['field1']
     };
 
     return search.query(indexName, {sort: [{field: 'field1'}]})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('builds sorted query with explicit order', () => {
-    const expected = {
-      index: indexName,
-      body: {
-        query: {filtered: {}},
-        size: 10,
-        sort: [{field1: {order: 'asc'}}]
-      }
+    const expectedQuery = {
+      query: {filtered: {}},
+      size: 10,
+      sort: [{field1: {order: 'asc'}}]
     };
 
     return search.query(indexName, {sort: [{field: 'field1', direction: 'asc'}]})
-      .then(() => expect(searchArgs).to.deep.equal(expected));
+      .then(() => expect(searchArgs.body).to.deep.equal(expectedQuery));
   });
 
   it('returns document source', () => {
