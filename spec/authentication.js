17faac8e67c1521f38c81566f4cfecf75be6a37c
Chris Impey <chris@bigwednesday.io>
2015-10-06 10:48:33 +0100

    require api key for get object from index so we can rebuild the index name

--- spec/authentication.js
+++ spec/authentication.js
@@ -3,12 +3,13 @@
 const expect = require('chai').expect;
 const specRequest = require('./spec_request');
 
-describe('endpoint authentcation', () => {
+describe('endpoint authentication', () => {
   const tests = [
     {routeName: '/1/indexes/{name}', method: 'POST', url: '/1/indexes/some-index'},
     {routeName: '/1/indexes/{name}', method: 'DELETE', url: '/1/indexes/some-index'},
     {routeName: '/1/indexes/{name}/settings', method: 'PUT', url: '/1/indexes/some-index/settings'},
     {routeName: '/1/indexes/{name}/settings', method: 'GET', url: '/1/indexes/some-index/settings'},
+    {routeName: '/1/indexes/{name}/{objectID}', method: 'GET', url: '/1/indexes/some-index/1}'},
     {routeName: '/1/indexes/{name}/{objectID}', method: 'DELETE', url: '/1/indexes/some-index/1}'},
     {routeName: '/1/indexes/{name}/{objectID}', method: 'PUT', url: '/1/indexes/some-index/1'},
     {routeName: '/1/indexes/{name}/batch', method: 'POST', url: '/1/indexes/some-index/batch'}
