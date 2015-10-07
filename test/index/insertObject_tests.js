173917693fd17fa86c5ccca0cb4298d5be405a45
Chris Impey <chris@bigwednesday.io>
2015-10-07 11:06:59 +0100

    create an index with a unique name before inserting if it doesn't exist

--- test/index/insertObject_tests.js
+++ test/index/insertObject_tests.js
@@ -7,57 +7,139 @@ const sinon = require('sinon');
 
 const expect = require('chai').expect;
 
-const testIndexName = 'my-index-name';
+const testNewIndexName = 'my-index-name';
+const testExistingIndexName = 'existing-index-name';
+
 const testObject = {name: 'an object', field1: 'value', field2: 0};
 
 describe('Search Index', () => {
   describe('insertObject', () => {
-    let elasticStub;
+    let indexStub;
+    let getAliasStub;
+    let createIndexStub;
+    let putAliasStub;
     let indexedObject;
-    let index;
+    let SearchIndex;
     let indexName;
+    let clock;
+    const testDate = new Date();
+    const indexNameDateString = `${testDate.getFullYear()}.${testDate.getMonth() + 1}.${testDate.getDate()}.${testDate.getMilliseconds()}`;
+
+    before(() => {
+      clock = sinon.useFakeTimers(testDate.getTime());
+    });
 
     beforeEach(() => {
-      elasticStub = sinon.stub(elasticsearchClient, 'index', args => {
+      indexStub = sinon.stub(elasticsearchClient, 'index', args => {
         indexedObject = args.body;
         indexName = args.index;
 
         return Promise.resolve({_id: '123'});
       });
 
-      const SearchIndex = require('../../lib/search_index');
-      index = new SearchIndex(testIndexName);
+      getAliasStub = sinon.stub(elasticsearchClient.indices, 'getAlias', args => {
+        if (args.name === testExistingIndexName) {
+          return Promise.resolve({
+            anIndex: {
+              aliases: {
+                [testExistingIndexName]: {}
+              }
+            }
+          });
+        }
+
+        if (args.ignore === 404) {
+          return Promise.resolve({});
+        }
+
+        const err = new Error();
+        err.error = `alias [${args.name}] missing`;
+        err.status = 404;
+
+        return Promise.reject(err);
+      });
+
+      createIndexStub = sinon.stub(elasticsearchClient.indices, 'create', () => {
+        return Promise.resolve({});
+      });
+
+      putAliasStub = sinon.stub(elasticsearchClient.indices, 'putAlias', () => {
+        return Promise.resolve({});
+      });
+
+      SearchIndex = require('../../lib/search_index');
     });
 
     afterEach(() => {
-      elasticStub.restore();
+      indexStub.restore();
+      getAliasStub.restore();
+      createIndexStub.restore();
+      putAliasStub.restore();
       indexedObject = undefined;
       indexName = undefined;
     });
 
-    it('adds the object to elasticsearch', () => {
-      return index.insertObject(testObject).then(() => {
-        expect(indexedObject).to.be.equal(testObject);
+    after(() => {
+      clock.restore();
+    });
+
+    describe('when the index does not exist', () => {
+      const expectedUniqueIndexName = `${testNewIndexName}_${indexNameDateString}`;
+
+      beforeEach(() => {
+        const index = new SearchIndex(testNewIndexName);
+        return index.insertObject(testObject);
+      });
+
+      it('creates the index with a unique name', () => {
+        sinon.assert.calledOnce(createIndexStub);
+        sinon.assert.calledWith(createIndexStub, {index: expectedUniqueIndexName});
+      });
+
+      it('sets the index name as an alias', () => {
+        sinon.assert.calledOnce(putAliasStub);
+        sinon.assert.calledWith(putAliasStub, {index: expectedUniqueIndexName, name: testNewIndexName});
       });
     });
 
-    it('writes to the named index', () => {
-      return index.insertObject(testObject).then(() => {
-        expect(indexName).to.be.equal(testIndexName);
+    describe('when the index exists', () => {
+      beforeEach(() => {
+        const index = new SearchIndex(testExistingIndexName);
+        return index.insertObject(testObject);
+      });
+
+      it('does not create a new index', () => {
+        sinon.assert.notCalled(createIndexStub);
+        sinon.assert.notCalled(putAliasStub);
       });
     });
 
-    it('returns the fields from the indexed object', () => {
-      return index.insertObject(testObject).then(o => {
-        _.forOwn(testObject, (value, property) => {
-          expect(o).to.have.property(property, value);
+    describe('insertion', () => {
+      let insertResult;
+
+      beforeEach(() => {
+        const index = new SearchIndex(testNewIndexName);
+        return index.insertObject(testObject).then(o => {
+          insertResult = o;
         });
       });
-    });
 
-    it('returns the generated objectID', () => {
-      return index.insertObject(testObject).then(o => {
-        expect(o.objectID).to.be.equal('123');
+      it('adds the object to elasticsearch', () => {
+        expect(indexedObject).to.be.equal(testObject);
+      });
+
+      it('writes to the named index', () => {
+        expect(indexName).to.be.equal(testNewIndexName);
+      });
+
+      it('returns the fields from the indexed object', () => {
+        _.forOwn(testObject, (value, property) => {
+          expect(insertResult).to.have.property(property, value);
+        });
+      });
+
+      it('returns the generated objectID', () => {
+        expect(insertResult.objectID).to.be.equal('123');
       });
     });
   });
