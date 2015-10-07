10dd4ed46c3019a860d8a5bc5aa8041740add4fe
Chris Impey <chris@bigwednesday.io>
2015-10-07 12:59:02 +0100

    create an index with unique name before performing batch creates and upserts, and don't run deletes if the index doesn't exist

--- test/index/batchOperation_tests.js
+++ test/index/batchOperation_tests.js
@@ -9,8 +9,10 @@ const testNewIndexName = 'my-index-name';
 const testExistingIndexName = 'existing-index-name';
 const operations = [
   {action: 'create', body: {name: 'an object'}},
-  {action: 'create', body: {name: 'another object'}}
+  {action: 'create', body: {name: 'another object'}},
+  {action: 'delete', objectID: '1'}
 ];
+const createIds = ['id1', 'id2'];
 
 describe('Search Index', () => {
   describe('batchOperation', () => {
@@ -18,8 +20,8 @@ describe('Search Index', () => {
     let createIndexStub;
     let putAliasStub;
     let bulkStub;
+    let bulkArgs;
     let SearchIndex;
-    let batchResult;
 
     beforeEach(() => {
       sandbox = sinon.sandbox.create();
@@ -54,10 +56,35 @@ describe('Search Index', () => {
         return Promise.resolve({});
       });
 
+      bulkStub = sandbox.stub(elasticsearchClient, 'bulk', args => {
+        bulkArgs = args;
+        let createdCount = 0;
+        const result = {};
+
+        result.items = args.body
+          .filter(row => {
+            return row.delete || row.index;
+          })
+          .map(header => {
+            if (header.delete) {
+              return {'delete': {_id: header.delete._id}};
+            }
+
+            if (header.index._id) {
+              return {index: {_id: header.index._id}};
+            }
+
+            return {create: {_id: createIds[createdCount++]}};
+          });
+
+        return Promise.resolve(result);
+      });
+
       SearchIndex = require('../../lib/search_index');
     });
 
     afterEach(() => {
+      bulkArgs = undefined;
       sandbox.restore();
     });
 
@@ -83,6 +110,16 @@ describe('Search Index', () => {
         sinon.assert.calledOnce(putAliasStub);
         sinon.assert.calledWith(putAliasStub, {index: expectedUniqueIndexName, name: testNewIndexName});
       });
+
+      it('does not run delete operations', () => {
+        sinon.assert.calledOnce(bulkStub);
+
+        const deleteRequest = bulkArgs.body.find(request => {
+          return request.delete;
+        });
+
+        expect(deleteRequest).to.not.exist;
+      });
     });
 
     describe('when the index exists', () => {
@@ -98,37 +135,20 @@ describe('Search Index', () => {
     });
 
     describe('create', () => {
+      let batchCreateResult;
+
       const batchOperations = [
         {action: 'create', body: {name: 'an object'}},
         {action: 'create', body: {name: 'another object'}}
       ];
 
-      const ids = ['id1', 'id2'];
-
-      before(() => {
-        bulkStub = sinon.stub(elasticsearchClient, 'bulk', () => {
-          const result = {
-            items: [{
-              create: {_id: ids[0]}
-            }, {
-              create: {_id: ids[1]}
-            }]
-          };
-
-          return Promise.resolve(result);
-        });
-
-        const SearchIndex = require('../../lib/search_index');
+      beforeEach(() => {
         const index = new SearchIndex(testNewIndexName);
 
         return index.batchOperation(batchOperations)
           .then(result => {
-            batchResult = result;
+            batchCreateResult = result;
           });
-      });
-
-      after(() => {
-        bulkStub.restore();
       });
 
       it('makes an index request for each created object', () => {
@@ -145,11 +165,12 @@ describe('Search Index', () => {
       });
 
       it('returns the ids of inserted objects', () => {
-        expect(batchResult.inserted).to.deep.equal(ids);
+        expect(batchCreateResult.inserted).to.deep.equal(createIds);
       });
     });
 
     describe('upsert', () => {
+      let bactchUpsertResult;
       const ids = ['id1', 'id2'];
 
       const batchOperations = [
@@ -157,25 +178,12 @@ describe('Search Index', () => {
         {action: 'upsert', objectID: ids[1], body: {name: 'another object'}}
       ];
 
-      before(() => {
-        bulkStub = sinon.stub(elasticsearchClient, 'bulk', () => {
-          const result = {
-            items: [{
-              index: {_id: ids[0]}
-            }, {
-              index: {_id: ids[1]}
-            }]
-          };
-
-          return Promise.resolve(result);
-        });
-
-        const SearchIndex = require('../../lib/search_index');
+      beforeEach(() => {
         const index = new SearchIndex(testNewIndexName);
 
         return index.batchOperation(batchOperations)
           .then(result => {
-            batchResult = result;
+            bactchUpsertResult = result;
           });
       });
 
@@ -197,11 +205,12 @@ describe('Search Index', () => {
       });
 
       it('returns the ids of upserted objects', () => {
-        expect(batchResult.upserted).to.deep.equal(ids);
+        expect(bactchUpsertResult.upserted).to.deep.equal(ids);
       });
     });
 
     describe('delete', () => {
+      let batchDeleteResult;
       const ids = ['id1', 'id2'];
 
       const batchOperations = [
@@ -209,38 +218,21 @@ describe('Search Index', () => {
         {action: 'delete', objectID: ids[1]}
       ];
 
-      before(() => {
-        bulkStub = sinon.stub(elasticsearchClient, 'bulk', () => {
-          const result = {
-            items: [{
-              'delete': {_id: ids[0]}
-            }, {
-              'delete': {_id: ids[1]}
-            }]
-          };
-
-          return Promise.resolve(result);
-        });
-
-        const SearchIndex = require('../../lib/search_index');
-        const index = new SearchIndex(testNewIndexName);
+      beforeEach(() => {
+        const index = new SearchIndex(testExistingIndexName);
 
         return index.batchOperation(batchOperations)
           .then(result => {
-            batchResult = result;
+            batchDeleteResult = result;
           });
-      });
-
-      after(() => {
-        bulkStub.restore();
       });
 
       it('makes a delete request for each deleted object', () => {
         const expectedBulkArgs = {
           body: [{
-            'delete': {_index: testNewIndexName, _type: 'object', _id: ids[0]}
+            'delete': {_index: testExistingIndexName, _type: 'object', _id: ids[0]}
           }, {
-            'delete': {_index: testNewIndexName, _type: 'object', _id: ids[1]}
+            'delete': {_index: testExistingIndexName, _type: 'object', _id: ids[1]}
           }]
         };
 
@@ -249,7 +241,7 @@ describe('Search Index', () => {
       });
 
       it('returns the ids of deleted objects', () => {
-        expect(batchResult.deleted).to.deep.equal(ids);
+        expect(batchDeleteResult.deleted).to.deep.equal(ids);
       });
     });
   });
